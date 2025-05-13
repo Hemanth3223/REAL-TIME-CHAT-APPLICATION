aea0574f7e24c261ff2ec40c2c3183765555ecf1
HemanthKumar S S <22ch015@kpriet.ac.in>
2025-05-13 14:41:48 +0530

    Update src.js

--- src.js
+++ src.js
@@ -5,3 +5,7 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<React.StrictMode><App /></React.StrictMode>);
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
